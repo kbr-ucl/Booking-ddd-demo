aa2c5c0b80de63c3163e568f6e5e715e9b8d69ee
Kaj Steen Bromose <KBR8776@ucl.dk>
2020-04-02 12:44:32 +0200

    Value objects done

--- src/Booking.Domain/Booking.cs
+++ src/Booking.Domain/Booking.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Framework;
 
 namespace Booking.Domain
 {
     public class Booking
     {
+        // Value
+        private BookingEnd _bookingEnd;
+
+        // Value
+        private BookingStart _bookingStart;
+
+        // Value
+        private BookingTypeId _bookingTypeId;
+
+        // Value
+        private StudentId _studentId;
+
+
+        // Value
+        private TimeSlotId _timeSlotId;
+
         public Booking(Guid id, BookingTypeId bookingTypeId, StudentId studentId, TimeSlotId timeSlotId)
         {
             _bookingTypeId = bookingTypeId;
@@ -14,21 +28,9 @@ namespace Booking.Domain
             Id = id;
             _timeSlotId = timeSlotId;
         }
+
         // Identifier
         public Guid Id { get; set; }
-        // Value
-        private BookingTypeId _bookingTypeId;
-        // Value
-        private StudentId _studentId;
-        
-        
-        // Value
-        private TimeSlotId _timeSlotId;
-
-        // Value
-        private BookingStart _bookingStart;
-        // Value
-        private BookingEnd _bookingEnd;
 
         public void UpdateTimeSlot(TimeSlotId timeSlotId)
         {
@@ -37,14 +39,13 @@ namespace Booking.Domain
 
         public void UpdateBookingStart(BookingStart bookingStart)
         {
-            _bookingStart= bookingStart;
+            _bookingStart = bookingStart;
         }
+
         public void UpdateBookingEnd(BookingEnd bookingEnd)
         {
-            _bookingEnd= bookingEnd;
+            _bookingEnd = bookingEnd;
         }
-
-
     }
 
     public class BookingEnd : Value<BookingEnd>
@@ -56,37 +57,103 @@ namespace Booking.Domain
         {
             if (bookingEnd == default)
                 throw new ArgumentException(
-                    "Booking start skal være sat",
+                    "Booking slut dato/tid skal være sat",
                     nameof(bookingEnd));
+
+            _bookingEnd = bookingEnd;
         }
 
         //internal BookingEnd(DateTime bookingEnd)
         //{
         //}
 
-        public new static BookingEnd FromDateTime(DateTime bookingEnd)
+        public static BookingEnd FromDateTime(DateTime bookingEnd)
         {
             return new BookingEnd(bookingEnd);
         }
     }
 
-    public class BookingStart
+    public class BookingStart : Value<BookingStart>
     {
         private DateTime _bookingStart;
+
+        private BookingStart(DateTime bookingStart)
+        {
+            if (bookingStart == default)
+                throw new ArgumentException(
+                    "Booking start dato/tid skal være sat",
+                    nameof(bookingStart));
+
+            _bookingStart = bookingStart;
+        }
+
+
+        public static BookingStart FromDateTime(DateTime bookingStart)
+        {
+            return new BookingStart(bookingStart);
+        }
     }
 
-    public class BookingTypeId
+    public class BookingTypeId : Value<BookingTypeId>
     {
         private Guid _bookingTypeId;
+
+        private BookingTypeId(Guid bookingTypeId)
+        {
+            if (bookingTypeId == default)
+                throw new ArgumentException(
+                    "Bookingtype skal være sat",
+                    nameof(bookingTypeId));
+
+            _bookingTypeId = bookingTypeId;
+        }
+
+
+        public static BookingTypeId FromGuid(Guid bookingTypeId)
+        {
+            return new BookingTypeId(bookingTypeId);
+        }
     }
 
     public class TimeSlotId
     {
         private Guid _timeSlotId;
+
+        private TimeSlotId(Guid timeSlotId)
+        {
+            if (timeSlotId == default)
+                throw new ArgumentException(
+                    "Time slot skal være sat",
+                    nameof(timeSlotId));
+
+            _timeSlotId = timeSlotId;
+        }
+
+
+        public static TimeSlotId FromGuid(Guid timeSlotId)
+        {
+            return new TimeSlotId(timeSlotId);
+        }
     }
 
-    public class StudentId
+    public class StudentId : Value<BookingTypeId>
     {
         private Guid _studentId;
+
+        private StudentId(Guid studentId)
+        {
+            if (studentId == default)
+                throw new ArgumentException(
+                    "StudentId skal være sat",
+                    nameof(studentId));
+
+            _studentId = studentId;
+        }
+
+
+        public static StudentId FromGuid(Guid studentId)
+        {
+            return new StudentId(studentId);
+        }
     }
 }
