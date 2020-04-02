b96ed91c62de1071c3e12c1dbc8ff5f3c102007f
Kaj Steen Bromose <KBR8776@ucl.dk>
2020-04-02 11:38:31 +0200

    First cut - props added in native types

---
+++ src/Booking.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Booking.Domain.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/Booking.Domain/Booking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booking.Domain
+{
+    public class Booking
+    {
+        // Identifier
+        public Guid Id { get; set; }
+        
+        // Value
+        public Guid StudentId { get; set; }
+        // Value
+        public Guid TimeSlotId { get; set; }
+        // Value
+        public Guid BookingTypeId { get; set; }
+        // Value
+        public DateTime BookingStart { get; set; }
+        // Value
+        public DateTime BookingEnd { get; set; }
+
+    }
+}
